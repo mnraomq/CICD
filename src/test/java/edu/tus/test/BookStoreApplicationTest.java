534fa3a4b54a4c77b469217ae9f8e267a5af5ac5
mnraomq <mnraomq@gmail.com>
2024-04-27 18:45:34 +0100

    version-1

--- src/test/java/edu/tus/test/BookStoreApplicationTest.java
+++ src/test/java/edu/tus/test/BookStoreApplicationTest.java
@@ -1,7 +1,5 @@
 package edu.tus.test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
