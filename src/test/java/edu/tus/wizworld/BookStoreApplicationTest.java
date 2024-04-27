ebcb2be8910b1d8bf877ba31240716d6b937742a
mnraomq <mnraomq@gmail.com>
2024-04-27 19:29:34 +0100

    version-1

--- src/test/java/edu/tus/wizworld/BookStoreApplicationTest.java
+++ src/test/java/edu/tus/wizworld/BookStoreApplicationTest.java
@@ -1,9 +1,7 @@
-package edu.tus.test;
+package edu.tus.wizworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import edu.tus.wizworld.HarryPotterBookstoreApplication;
 
 @SpringBootTest(classes = HarryPotterBookstoreApplication.class)
 public class BookStoreApplicationTest {
--- src/test/java/edu/tus/wizworld/BookValidatorTest.java
+++ src/test/java/edu/tus/wizworld/BookValidatorTest.java
@@ -1,4 +1,4 @@
-package edu.tus.test;
+package edu.tus.wizworld;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
