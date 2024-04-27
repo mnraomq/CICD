a1a71fa13f2e9a4830ef2b2dea26240763cb4c66
mnraomq <mnraomq@gmail.com>
2024-04-27 20:10:09 +0100

    version-1

---
+++ src/test/java/edu/tus/wizworld/BookStoreApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.tus.wizworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = HarryPotterBookstoreApplication.class)
+public class BookStoreApplicationTest {
+	
+	@Test
+	public void contextLoads() {
+		HarryPotterBookstoreApplication.main(new String[]{});
+	}	
+}
